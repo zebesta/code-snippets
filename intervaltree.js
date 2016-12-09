bc227b37b1fd452d1b4566e0ab7a6f8ad35aa3d3
zebesta <chris.sebesta@gmail.com>
2016-12-09 16:57:13 -0500

    fixed remove

--- intervaltree.js
+++ intervaltree.js
@@ -79,8 +79,63 @@ function findIntersection(interval, root) {
 
 }
 
-function removeInterval () {
+function removeInterval (node) {
+  var left = false;
+  //right now deleting whole tree, need to actually return a valid tree with root removed
+  if(node.parent == null){
+    treeRoot = null;
+    return;
+  }else{
+    var parent = node.parent;
+    // console.log(parent);
+    left = (parent.left == node);
+  }
 
+  //case no childresn
+  if(node.right == null && node.left ==null){
+    console.log("Only child");
+    if(left){
+      console.log("removing left");
+      node.parent.left = null;
+    }else {
+      console.log("removing right");
+      node.parent.right = null;
+    }
+    return;
+  }
+  // find min of right sub tree and replace this node with it, then delete its place
+  if(node.right != null){
+    console.log("finding min of right sub");
+    var replacer = node.right;
+    while(replacer.left){
+      replacer = replacer.left;
+    }
+    if(left){
+      node.parent.left = replacer;
+    }else {
+      node.parent.right = replacer;
+    }
+    //remove replacer from its current place in tree
+    removeInterval(replacer);
+    //update this node parent
+    replacer.parent = node.parent;
+  }else if(node.left != null){
+    console.log("replacing with left")
+    if(left){
+      node.parent.left = node.left;
+    }else {
+      node.parent.right = node.left;
+    }
+  }else{
+    //Only child, replace with left node
+    if(left){
+      node.parent.left = node.left;
+      node.left.parent = node.parent;
+    }else {
+      node.parent.right = node.left;
+      node.left.parent = node.parent;
+    }
+  }
 }
 
 var a = new Interval(15,7);
@@ -89,6 +144,13 @@ var n = new Node();
 
 addInterval(new Interval(5,8), treeRoot);
 addInterval(new Interval(12,19), treeRoot);
+addInterval(new Interval(14,19), treeRoot);
+addInterval(new Interval(7,13), treeRoot);
 addInterval(new Interval(2,4), treeRoot);
-console.log(findIntersection(new Interval(14,18), treeRoot));
-console.log(findIntersection(new Interval(50,70), treeRoot));
+addInterval(new Interval(1,4), treeRoot);
+
+// console.log(findIntersection(new Interval(14,18), treeRoot));
+// console.log(findIntersection(new Interval(50,70), treeRoot));
+console.log(treeRoot)
+removeInterval(treeRoot.right);
+console.log(treeRoot)
