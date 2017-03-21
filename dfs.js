05e33ced2ce9b8b909ceb1c47a56ee6f1bf8a758
zebesta <chris.sebesta@gmail.com>
2017-03-21 10:38:41 -0400

    graph dfs with pathing

---
+++ dfs.js
@@ -0,0 +1,66 @@
+class Graph {
+  constructor () {
+    this.graph = {}
+  }
+
+  // add a directed adge from a to b, create node(s) if necessary
+  addEdge (a, b) {
+    if(!this.graph[a]){
+      this.graph[a] = [];
+    }
+    if(!this.graph[b]){
+      this.graph[b] = [];
+    }
+    this.graph[a].push(b);
+  }
+
+  // BFS class method
+  bfs(a) {
+    var visited = {};
+    var queue = [];
+
+    queue.push(a);
+
+    while(queue.length > 0){
+      console.log(queue);
+      var current = queue.shift();
+      console.log(current);
+      visited[current] = true;
+
+      var neighbors = this.graph[current];
+      for(let n of neighbors){
+        if(!visited[n]){
+          // console.log("Adding " + n + " to the queue");
+          queue.push(n);
+          this.graph[n].from = current;
+        }
+      }
+    }
+  }
+
+  path(a, b) {
+    this.bfs(a);
+    console.log("PATH BETWEEN " + a + " and " + b);
+    var path = [];
+    while(b !== a){
+      path.unshift(b);
+      b = this.graph[b].from;
+    }
+    console.log(path);
+  }
+}
+
+var g = new Graph();
+g.addEdge(0, 1);
+g.addEdge(0, 2);
+g.addEdge(1, 2);
+g.addEdge(2, 0);
+g.addEdge(2, 3);
+g.addEdge(3, 3);
+g.addEdge(1, 4);
+g.addEdge(4, 5);
+g.addEdge(2,5);
+
+console.log("Following is Breadth First Traversal (starting from vertex 2)");
+g.bfs(2);
+g.path(2, 5);
